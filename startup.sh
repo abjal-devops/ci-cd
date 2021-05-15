f12edec8d97858b91eb9c16a8db656db7f929ebb
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2021-05-15 19:39:41 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/local/tomcat/bin/catalina.sh run
 sleep 15
-mv /usr/local/tomcat/webapps/java-web-project ROOT
+mv /usr/local/tomcat/webapps/java-web-project /usr/local/tomcat/webapps/ROOT
 
 
